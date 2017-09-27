fa50dab907cd3753e7440401bc19a2e0321c56f7
Anais Kim <itschstyle@gmail.com>
2017-09-27 17:25:30 +0900

    Change stub-runner-boot.jar & add sample testcase

--- test/sample.js
+++ test/sample.js
@@ -1,11 +1,27 @@
 var request = require('supertest')
 
-it('GET /folders', function(done){
-    request('http://localhost:8787')
-      .get('/api/v1/folders')
-      .set('Content-Type', 'application/json')
-      .query({ type: 'root' })
-      .expect(200, {
-        id: '334535'
-      }, done);
+var app = 'http://localhost:8787';
+
+describe('GET /folders', function () {
+  it('should return folder id', function(done){
+      request(app)
+        .get('/api/v1/folders')
+        .set('Content-Type', 'application/json')
+        .query({ type: 'root' })
+        .expect(200, {
+          id: '334535'
+        }, done);
+  });
+});
+
+describe('GET /media/{id}', function () {
+  it('should return media id and type', function(done){
+      request(app)
+        .get('/api/v1/media/111')
+        .set('Content-Type', 'application/json')
+        .expect(200, {
+          id: '111',
+          type: 'image'
+        }, done);
+  });
 });
