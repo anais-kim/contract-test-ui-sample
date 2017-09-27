6ce658251d07b8f154b4f29a6a2499dafdfc7e47
actmember <actmember@actmembers-iMac-2.local>
2017-09-27 19:40:40 +0900

    add test validation

--- test/contract-content.js
+++ test/contract-content.js
@@ -1,13 +1,14 @@
 var request = require('supertest')
-var assert = require('assert')
 
-var app = 'http://localhost:8989';
+var contentApp = 'http://localhost:8989';
 
 describe('GET /playlists/{id}', function () {
   it('should return display orientaion', function(done){
-      request(app)
+      request(contentApp)
         .get('/api/v1/playlists/888')
         .set('Content-Type', 'application/json')
-        .expect(200, done);
+        .expect(200, {
+            display: {'orientation': 'vertical'}
+        }, done);
   });
 });
--- test/contract-file.js
+++ test/contract-file.js
@@ -1,10 +1,10 @@
 var request = require('supertest')
 
-var app = 'http://localhost:8787';
+var fileApp = 'http://localhost:8787';
 
 describe('GET /folders', function () {
   it('should return folder id', function(done){
-      request(app)
+      request(fileApp)
         .get('/api/v1/folders')
         .set('Content-Type', 'application/json')
         .query({ type: 'root' })
@@ -16,7 +16,7 @@ describe('GET /folders', function () {
 
 describe('GET /media/{id}', function () {
   it('should return media id and type', function(done){
-      request(app)
+      request(fileApp)
         .get('/api/v1/media/111')
         .set('Content-Type', 'application/json')
         .expect(200, {
